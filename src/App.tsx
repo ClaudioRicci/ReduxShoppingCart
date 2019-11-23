f55d697cc0010f2403979e8ae832cfb308a55b34
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-11-23 23:26:37 +0000

    reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,23 @@
 import React, { Component } from "react";
-import Product from "./components/Product";
+import Products from "./components/Products";
 import productsData from "./data/ProductsData";
 
-const products = productsData;
+const data = productsData;
 type AppProps = {};
 type AppState = { name: string; cart: any };
 
 class App extends Component<AppProps, AppState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      name: "React",
-      cart: [
-        {
-          id: 1,
-          name: "Beans",
-          description: "Baked Beans",
-          img: "./images/beans.jpg",
-          price: 0.5,
-          onOffer: true,
-          offerQuantityNeeded: 3,
-          offerPrice: 0.33,
-          byWeight: false,
-          individualWeight: 150,
-          volumeType: "kg",
-          units: 1
-        }
-      ]
-    };
-  }
-
-  handleAddFunc(product) {
-    const existingProductIndex = this.state.cart.findIndex(
-      p => p.id === product.id
-    );
-
-    if (existingProductIndex >= 0) {
-      const cartProducts = this.state.cart.slice();
-
-      const existingProduct = cartProducts[existingProductIndex];
-
-      const updatedUnitsProduct = {
-        ...existingProduct,
-        units: existingProduct.units + product.units
-      };
-
-      cartProducts[existingProductIndex] = updatedUnitsProduct;
-
-      this.setState({
-        cart: cartProducts
-      });
-    } else {
-      this.setState({
-        cart: [...this.state.cart, product]
-      });
-    }
-  }
-
   render() {
     return (
       <main>
-        <ul>
+        {/* <ul>
           {this.state.cart.map(item => (
             <li>
               {item.name} | {item.units}
             </li>
           ))}
-        </ul>
-        {products.map(product => (
-          <Product
-            key={product.id}
-            {...product}
-            addFunc={this.handleAddFunc.bind(this)}
-          />
-        ))}
+        </ul> */}
+        <Products products={data} />
       </main>
     );
   }
